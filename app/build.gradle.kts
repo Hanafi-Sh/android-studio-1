f6ffe81bed4dd13a04b3862d531dc3b94fb5bc31
Han <hanafishiddiq2@gmail.com>
2026-03-10 20:36:30 +0700

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.hanafi.han"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.hanafi.han"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    // Library untuk WorkManager (Si Ninja Latar Belakang)
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    // Library untuk membaca GPS dari Google Play Services
+    implementation("com.google.android.gms:play-services-location:21.2.0")
+    // Library agar sinkronisasi lebih mudah tanpa RAM bocor
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+}
