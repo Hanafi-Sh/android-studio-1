ad21325e1a2cdb9f4fb6ae4e7517d47e9bcd256a
Hanafi-Sh <hanafishiddiq2@gmail.com>
2026-03-11 11:28:29 +0700

    AutoStart

---
+++ app/src/main/java/com/hanafi/han/BootReceiver.kt
@@ -0,0 +1,29 @@
+package com.hanafi.han
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // Jika sinyal yang diterima adalah "HP Selesai Menyala"
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            Log.d("PelacakNinja", "HP menyala! Membangkitkan ulang radar pelacak...")
+
+            // Siapkan ulang jadwal pekerja (WorkManager) persis seperti di MainActivity
+            val requestPelacak = PeriodicWorkRequestBuilder<LocationWorker>(15, TimeUnit.MINUTES).build()
+            
+            // Masukkan kembali ke sistem Android
+            WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+                "PelacakLokasi",
+                ExistingPeriodicWorkPolicy.KEEP, 
+                requestPelacak
+            )
+        }
+    }
+}
