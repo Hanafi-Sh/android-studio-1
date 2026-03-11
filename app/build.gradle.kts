2d16952fbf8d77f6b9f91408b15275bf4430967b
Han <hanafishiddiq2@gmail.com>
2026-03-12 05:28:40 +0700

    Add persistent debug keystore for seamless updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,19 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
